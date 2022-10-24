c10ef8e49993248c01453e03693f81e78676ee68
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-10-24 10:44:04 +0000

    working on file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
-
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+    <Router>
+      <div className= "container">
+         <p>Let's add Routing!</p>
+      </div>
+      <switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </switch>
+   </Router>
+        );
 }
