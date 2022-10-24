9ca2adcc9d45749821f39f82770bfa47dd5e36df
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-10-24 18:26:10 +0000

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-export default function App() {
+ function App() {
   return (
     <Router>
-      <div className= "container">
+      <div className= 'container'>
          <p>Let's add Routing!</p>
       </div>
       <switch>
